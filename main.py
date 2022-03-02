c0b4ce5a98a4e32a5b5023550716e9c206cac705
Thomas Le Menestrel <tlmenest@stanford.edu>
2022-03-02 14:39:54 -0800

    Added sklearn models

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from sklearn.metrics import mean_squared_error
 from sklearn.decomposition import PCA
 
 # Code
-from eda import pearson_corr_mat, get_corr_features
-from models import LinearRegression, RidgeRegression, LassoRegression, log_loss
-from utils import separate_xy, min_max_normalization, mean_normalization, vif_feature_selection
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
+#from models import LinearRegression, RidgeRegression, LassoRegression, log_loss
+from models import log_loss
+from utils import separate_xy, min_max_normalization, mean_normalization, vif_feature_selection, get_corr_features
 
 ################################################################################
 # DATA PRE-PROCESSING
@@ -49,9 +50,10 @@ print("VIF selected features:")
 print(selected_features)
 
 ################################################################################
-# REGRESSION MODELS
+# REGRESSION MODELS BY HAND
 ################################################################################
 
+'''
 print()
 print('#######################################################################')
 print('LINEAR REGRESSION')
@@ -98,6 +100,57 @@ print(lasso_reg.beta)
 print()
 print("Lasso regression loss: ")
 print(log_loss(y_pred_lasso, y_train))
+print()
+
+print("Predictions:")
+print(y_pred_linear)
+
+print("Actual values:")
+print(y_train)
+'''
+
+################################################################################
+# REGRESSION MODELS SKLEARN
+################################################################################
+
+print()
+print('#######################################################################')
+print('LINEAR REGRESSION')
+print('#######################################################################')
+print()
+
+linear_reg = LinearRegression()
+linear_reg.fit(X_train, y_train)
+y_pred_linear = linear_reg.predict(X_train)
+
+print("Linear regression loss: ")
+print(mean_squared_error(y_pred_linear, y_train))
+
+print()
+print('#######################################################################')
+print('RIDGE REGRESSION')
+print('#######################################################################')
+print()
+
+ridge_reg = Ridge(alpha=1)
+ridge_reg.fit(X_train, y_train)
+y_pred_ridge = ridge_reg.predict(X_train)
+
+print("Ridge regression loss: ")
+print(mean_squared_error(y_pred_ridge, y_train))
+
+print()
+print('#######################################################################')
+print('LASSO REGRESSION')
+print('#######################################################################')
+print()
+
+lasso_reg = Lasso(alpha=1)
+lasso_reg.fit(X_train, y_train)
+y_pred_lasso = lasso_reg.predict(X_train)
+
+print("Lasso regression loss: ")
+print(mean_squared_error(y_pred_lasso, y_train))
 print()
 
 print("Predictions:")
